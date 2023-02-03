36220ecad314a51e086531b1bb7c6aeabd6adccc
hattie <hattie@cty.com>
2023-02-03 22:14:05 +0800

    3st add userinfo

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore, } from 'vuex'
-import number from './state/numState.js'
+import number from './modules/num.js'
+import userInfo from './modules/userInfo.js'
 
 export default createStore({
   modules: {
-    number
+    number,
+    userInfo
   }
 })
